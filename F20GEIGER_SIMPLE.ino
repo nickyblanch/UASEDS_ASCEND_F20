ac5ac804f16503f07f79b15d6c86f734243155ab
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2020-10-18 23:16:46 -0700

    Add files via upload
    This is the updated flight code that supports two geiger counters. TODO: Add BME680 code.

--- F20GEIGER_SIMPLE.ino
+++ F20GEIGER_SIMPLE.ino
@@ -1,21 +1,33 @@
 // INITIALIZE HARDWARE
-int signPin = 2;
-int noisePin = 3;
+int signPin_1 = 2;
+int noisePin_1 = 3;
+int signPin_2 = 4;
+int noisePin_2 = 5;
+
 // MEASURMENT VARIAVLES
-int sign = 0;
-int noise = 0;
-int sON = 0;
-int nON = 0;
-int signCount = 0;
-int noiseCount = 0;
+int sign_1 = 0;
+int noise_1 = 0;
+int sign_2 = 0;
+int noise_2 = 0;
+int sON_1 = 0;
+int nON_1 = 0;
+int sON_2 = 0;
+int nON_2 = 0;
+int signCount_1 = 0;
+int noiseCount_1 = 0;
+int signCount_2 = 0;
+int noiseCount_2 = 0;
+
 // TIME VARIABLES
 unsigned long int total_time = 0;
 unsigned long int currTime = 0;
 unsigned long int prevTime = 0;
 unsigned int diffTime = 0;
-int loop_index = 0;
+long int loop_index = 0;
 // CPM
-double cpm = 0;
+
+double cpm_1 = 0;
+double cpm_2 = 0;
 
 void setup() {
   //Serial setup
@@ -23,66 +35,111 @@ void setup() {
   Serial.begin(9600);
 
   //PIN setting for Radiation Pulse
-  pinMode(signPin, INPUT);
-  digitalWrite(signPin, HIGH);
+  pinMode(signPin_1, INPUT);
+  digitalWrite(signPin_1, HIGH);
+  pinMode(signPin_2, INPUT);
+  digitalWrite(signPin_2, HIGH);
 
   //PIN setting for Noise Pulse
-  pinMode(noisePin, INPUT);
-  digitalWrite(noisePin, HIGH);
+  pinMode(noisePin_1, INPUT);
+  digitalWrite(noisePin_1, HIGH);
+  pinMode(noisePin_2, INPUT);
+  digitalWrite(noisePin_2, HIGH);
 
   prevTime = millis();
+
+  // INITIALIZE SERIAL OUTOUT
+  Serial.println("[time, ms] [GC1] [GC2]");
 }
 
 void loop() {
+    //////////////
+    // GEIGER 1 //
+    //////////////
    // Raw data of Radiation Pulse: Not-detected -> High, Detected -> Low
-  int sign = digitalRead(signPin);
-
-  // Raw data of Noise Pulse: Not-detected -> Low, Detected -> High
-  int noise = digitalRead(noisePin);
-
+  int sign_1 = digitalRead(signPin_1);
+  int noise_1 = digitalRead(noisePin_1);
    //Radiation Pulse normally keeps low for about 100[usec]
-  if (sign == 0 && sON == 0)
+  if (sign_1 == 0 && sON_1 == 0)
   { //Deactivate Radiation Pulse counting for a while
-    sON = 1;
-    signCount++;
+    sON_1 = 1;
+    signCount_1++;
     Serial.println("Yes!");
-  } else if (sign == 1 && sON == 1) {
-    sON = 0;
+  } else if (sign_1 == 1 && sON_1 == 1) {
+    sON_1 = 0;
+  }
+  //Noise Pulse normally keeps high for about 100[usec]
+  if (noise_1 == 1 && nON_1 == 0)
+  { //Deactivate Noise Pulse counting for a while
+    nON_1 = 1;
+    noiseCount_1++;
+  } else if (noise_1 == 0 && nON_1 == 1) {
+    nON_1 = 0;
   }
 
+     //////////////
+    // GEIGER 2 //
+    //////////////
+  // Raw data of Noise Pulse: Not-detected -> Low, Detected -> High
+  int sign_2 = digitalRead(signPin_2);
+  int noise_2 = digitalRead(noisePin_2);
+   //Radiation Pulse normally keeps low for about 100[usec]
+  if (sign_2 == 0 && sON_2 == 0)
+  { //Deactivate Radiation Pulse counting for a while
+    sON_2 = 1;
+    signCount_2++;
+  } else if (sign_2 == 1 && sON_2 == 1) {
+    sON_2 = 0;
+  }
   //Noise Pulse normally keeps high for about 100[usec]
-  if (noise == 1 && nON == 0)
+  if (noise_2 == 1 && nON_2 == 0)
   { //Deactivate Noise Pulse counting for a while
-    nON = 1;
-    noiseCount++;
-    
-  } else if (noise == 0 && nON == 1) {
-    nON = 0;
+    nON_2 = 1;
+    noiseCount_2++;
+  } else if (noise_2 == 0 && nON_2 == 1) {
+    nON_2 = 0;
   }
 
   //Output readings to serial port, after 10000 loops
-  if (loop_index == 10000) //About 160-170 msec in Arduino Nano(ATmega328)
+  if (loop_index == 100000) //About 100 * 160-170 msec in Arduino Nano(ATmega328)
   {
     //Get current time
     currTime = millis();
 
+    // GEIGER 1
     //No noise detected in 10000 loops
-    if (noiseCount == 0)
+    if (noiseCount_1 == 0)
     {
       diffTime = currTime - prevTime;
-      cpm = ((1.0 * noiseCount) / diffTime) * 60000.0;
+      cpm_1 = ((1.0 * signCount_1) / diffTime) * 60000.0;
     }
     else {
-      cpm = -1;
+      cpm_1 = -1;
     }
-    Serial.print(currTime);
-    Serial.print(" ");
-    Serial.println(cpm);
 
-    // RESER VARIABLES
-    cpm = 0;
-    noiseCount = 0;
-    signCount = 0;
+    // GEIGER 2
+    //No noise detected in 10000 loops
+    if (noiseCount_2 == 0)
+    {
+      diffTime = currTime - prevTime;
+      cpm_2 = ((1.0 * signCount_2) / diffTime) * 60000.0;
+    }
+    else {
+      cpm_2 = -1;
+    }
+    Serial.print(total_time);
+    Serial.print(" ");
+    Serial.print(cpm_1);
+    Serial.print(" ");
+    Serial.println(cpm_2);
+
+    // RESET VARIABLES
+    cpm_1 = 0;
+    cpm_2 = 0;
+    noiseCount_1 = 0;
+    signCount_1 = 0;
+    noiseCount_2 = 0;
+    signCount_2 = 0;
     loop_index = 0;
     prevTime = millis();
     total_time = millis();
